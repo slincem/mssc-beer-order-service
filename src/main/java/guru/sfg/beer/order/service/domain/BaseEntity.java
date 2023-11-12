c496c4253301dba19d27d2fddbb2b7bb742f82c0
Santiago Montano <slincem@gmail.com>
2023-11-11 20:54:30 -0500

    Fixing Spring versions

--- src/main/java/guru/sfg/beer/order/service/domain/BaseEntity.java
+++ src/main/java/guru/sfg/beer/order/service/domain/BaseEntity.java
@@ -24,7 +24,7 @@ import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.Type;
 import org.hibernate.annotations.UpdateTimestamp;
 
-import javax.persistence.*;
+import jakarta.persistence.*;
 import java.sql.Timestamp;
 import java.util.UUID;
 
@@ -51,7 +51,6 @@ public class BaseEntity {
             name = "UUID",
             strategy = "org.hibernate.id.UUIDGenerator"
     )
-    @Type(type="org.hibernate.type.UUIDCharType")
     @Column(length = 36, columnDefinition = "varchar", updatable = false, nullable = false )
     private UUID id;
 
