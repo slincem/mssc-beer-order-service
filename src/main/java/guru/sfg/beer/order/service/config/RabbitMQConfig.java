29a96e01ffd3c8b780534e9d1930d49865bb3cd6
Santiago Montano <slincem@gmail.com>
2023-12-18 23:21:50 -0500

    Adding RabbitMQ first config

---
+++ src/main/java/guru/sfg/beer/order/service/config/RabbitMQConfig.java
@@ -0,0 +1,33 @@
+package guru.sfg.beer.order.service.config;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    public static final String TOPIC_EXCHANGE_NAME = "beer-exchange";
+    public static final String QUEUE_NAME = "beer-orders";
+
+    @Bean
+    Queue queue() {
+        return new Queue(QUEUE_NAME, false);
+    }
+
+    @Bean
+    TopicExchange exchange() {
+        return new TopicExchange(TOPIC_EXCHANGE_NAME);
+    }
+
+    @Bean
+    Binding binding(Queue queue, TopicExchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange).with("beer.#");
+    }
+}
